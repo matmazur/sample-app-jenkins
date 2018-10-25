35ce243ee46958f3fef8aab4ab0bc346a0dc403e
Bingo Bobo <matmazur90@gmail.com>
2018-10-25 21:27:15 +0200

    added quick servlet taking info from form

---
+++ src/main/java/TestServlet.java
@@ -0,0 +1,30 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/test-servlet")
+public class TestServlet  extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String name = req.getParameter("name");
+        String pass = req.getParameter("password");
+
+
+        PrintWriter wr = resp.getWriter();
+
+        wr.println("me hehe. your credentials are\n");
+
+        wr.println("username: "+name);
+        wr.println("password: "+pass);
+
+        wr.println("\nthat was soooo unwise.. boo hoo ");
+
+
+    }
+}
